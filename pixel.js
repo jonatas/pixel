515ec1be8f8eb6e8f9b1861bd18625fdc04466e5
Jônatas Paganini <jonatasdp@gmail.com>
2013-04-03 23:48:10 -0300

    it move pixels and work with multiple sizes and colors

--- pixel.js
+++ pixel.js
@@ -1,18 +1,46 @@
+Screen = new Meteor.Collection("screen");
+
+function now() {
+  return new Date().getTime();
+}
+function randomColor(){
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to pixel.";
+  Meteor.subscribe('screen');
+  Template.screen.cursors = function() {
+    return Screen.find();
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.cursorconf.events({
+    'change select#size' : function(evt) {
+      Screen.update(Session.get("cursor"), {$set: {size: evt.target.value}});
+    },
+    'change select#currentColor': function(evt) {
+      var color = evt.target.value;
+      if (color == "random"){
+        color = randomColor();
+      }
+      Screen.update(Session.get("cursor"), {$set: {color: color}});
+      console.log("new color: ",color);
     }
   });
-}
 
+  Meteor.startup(function () {
+     Session.set('userId', Meteor.uuid());
+     Session.set("cursor", Screen.insert({userId: Session.get("userId"), color: randomColor(), x: 100, y: 100, size: 4}));
+     d3.select('html').on('mousemove', function()
+       {
+         var coordinates = d3.mouse(this);
+         Screen.update(Session.get("cursor"), {$set: { x: coordinates[0], y: coordinates[1]}});
+       }
+     );
+  });
+}
 if (Meteor.isServer) {
+  Meteor.publish('screen', function () {
+    return Screen.find();
+  });
   Meteor.startup(function () {
     // code to run on server at startup
   });
